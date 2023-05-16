59541702d339e08ddc3cef476658f43cd478951d
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-05-16 21:51:37 +0200

    Add AppIcon Set Launch Screen

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Teenu Abraham on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
