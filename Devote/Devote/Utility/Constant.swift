507c136a79e8c2dad3e1fd461165b5fa377a8e45
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-05-22 22:07:27 +0200

    Setup the Core Data

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Teenu Abraham on 22/05/23.
+//
+
+import SwiftUI
+
+ // MARK: - FORMATTER
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+ // MARK: - UI
+
+ // MARK: - UX
