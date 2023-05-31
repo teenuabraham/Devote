1e4e5c3871467c9cb2c840b8b413cb65e4284351
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-05-31 22:32:10 +0200

    Custom visual effect

--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -118,11 +118,16 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 }//: VSTACK
-                
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5), value: 0)
                  // MARK: - NEW TASK ITEM
                 
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ?  Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5
+                    )
                         .onTapGesture {
                             withAnimation(){
                                 showNewTaskItem = false
@@ -140,6 +145,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0 , opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
