62b82cf9785377dcef811b0f1ccf38aa072d414b
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-06-02 21:22:39 +0200

    Add Widget

---
+++ Devote/DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Teenu Abraham on 01/06/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
